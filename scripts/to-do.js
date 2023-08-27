41694a7e10962903340f4ec8d969b8bcf8273144
Sean Sutter <74794351+seanwsutter@users.noreply.github.com>
2023-08-27 02:04:57 +0000

    list

--- scripts/to-do.js
+++ scripts/to-do.js
@@ -2,23 +2,31 @@
 // when we click add, get text from textbox, add it to array, log the array
 
 // add html to display todos
-const toDoList = ['change diaper', 'dump'];
+const toDoList = [
+  {name: 'task', dueDate: '2023-1-1'},
+  {name: 'task', dueDate: '2023-1-1'}
+]; 
 renderToDoList();
 function renderToDoList () { // render, display on page
   let toDoListHTML = '';
   for (let i=0; i<toDoList.length; i++) {
-    const toDo = toDoList[i];
-    const html = `<p>${toDo}</p>`
+    const toDoObject = toDoList[i];
+    const {name, dueDate} = toDoObject;
+    const html = `<div>${name}</div> <div>${dueDate}</div>
+      <button onclick="toDoList.splice(${i}, 1); renderToDoList();" class="deleteToDoButton">Delete</button>`;
     toDoListHTML += html;
   }
-  console.log(toDoListHTML);
+  
   document.querySelector('.jsToDoList')
     .innerHTML = toDoListHTML;
   }
 function addToDo() {
   const inputElement = document.querySelector('.jsNameInput'); // gets input element and puts in js
   const name = inputElement.value // create variable to store input
-  toDoList.push(name)
+  const dateInputElement = document.querySelector('.jsDueDateInput')
+  const dueDate = dateInputElement.value;
+  // toDoList.push({name: name, dueDate: dueDate});
+  toDoList.push({name, dueDate});
   console.log(toDoList);
   inputElement.value= '';
   renderToDoList();
